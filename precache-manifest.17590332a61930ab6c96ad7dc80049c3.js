edd0353d8f87b20cf153847a08e67d9e8b8a863b
Ibochkarev <ivanx86@gmail.com>
2020-02-14 00:52:40 +0600

    Updates

---
+++ precache-manifest.17590332a61930ab6c96ad7dc80049c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a37d4f2b9f60e1ea2c4976bb255979d",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "a39042405bd2353624c6",
+    "url": "/crwn-clothing/static/css/main.e7010cc6.chunk.css"
+  },
+  {
+    "revision": "61acdf975dbc77b29bdb",
+    "url": "/crwn-clothing/static/js/2.0b6bf41b.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/crwn-clothing/static/js/2.0b6bf41b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a39042405bd2353624c6",
+    "url": "/crwn-clothing/static/js/main.3d70c3c1.chunk.js"
+  },
+  {
+    "revision": "0476b8ec577c2ae33b1e",
+    "url": "/crwn-clothing/static/js/runtime-main.844656b4.js"
+  }
+]);
