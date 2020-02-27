c11ffcdb3aa56a8963fd1ed2976002f37f8181f4
Ibochkarev <ivanx86@gmail.com>
2020-02-27 14:31:07 +0600

    Added firestore

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import "./collection-item.styles.scss";
 
 const CollectionItem = ({ id, name, price, imageUrl }) => (
-  <div className="collection-item" key={id}>
+  <div className="collection-item">
     <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
     <div className="collection-footer">
       <div className="name">{name}</div>
